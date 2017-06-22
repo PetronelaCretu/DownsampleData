529720c64a7380b1d29e4efd8a2e431790e697e1
Petronela Cretu <Petronela.Cretu@mcl.at>
2017-06-22 15:41:40 +0200

    updates to dictionary class

--- types/extended_dictionary.py
+++ types/extended_dictionary.py
@@ -6,34 +6,36 @@ Created on 22.06.2017
 from decorators.try_function import tryFunction
 
 
-class Dictionary():
+class Dictionary(dict):
     '''
     wrapper for dict that extends dict functionality
     the class can take an existing dict as object if one given at instantiation,
     otherwise a new empty dict is created
     '''
     
-    def __init__(self, dicty = None):
-        if dicty:
-            self._dict = dicty 
-        else:
-            self._dict = dict()
+    def __init__(self):
+        super().__init__()
     
     @tryFunction
     def convertValuesToFloat(self):
         '''
         the dict object will be copied and refiled with the converted data
         '''
-        from copy import deepcopy
-        data = deepcopy(self._dict)
-        self._dict = {}
+        self.values() = map(float, self.values())
         
-        for key in data.keys():
-            try:
-                # convert string values to float for a whole array
-                self._dict.update({key:list(map(float, data[key])) })
-            except Exception as e: 
-                print(e, 'Error: key ', key , 'in file ', self.fileName, ', array will not be saved', self.fileName, ' has values not valid')
-                continue
+#         from copy import deepcopy
+#         data = deepcopy(self)
+#         self = {}
+#         
+#         for key in self.keys():
+#             try:
+#                 # convert string values to float for a whole array
+#                 self.update({key:list(map(float, data[key])) })
+#             except Exception as e: 
+#                 print(e, 'Error: key ', key , 'in file ', self.fileName, ', array will not be saved', self.fileName, ' has values not valid')
+#                 continue
+
+
+
 
         
