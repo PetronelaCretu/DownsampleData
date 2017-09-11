49568fcaa385be67365b5926c5fe1f67861822af
PetronelaCretu <petronela.cretu@gmail.com>
2017-09-11 16:11:28 +0200

    Delete __init__.py
    not needed

--- plots_displays/__init__.py
+++
@@ -1,7 +0,0 @@
-__author__ = "Petronela Cretu"
-__copyright__ = "Copyright 2017, Materials Center Leoben Forschung GmbH"
-__credits__ = ["Manfred Mücke"]
-__license__ = "All rights reserved"
-__version__ = "0.0.1"
-__email__ = "petronela.cretu@mcl.at"
-__status__ = "Development"
